b4b4c466dfe4d4a056f1e6d3b401a96ad2daa7e4
jolielola-sys <joely.cornwall@gmail.com>
2026-07-26 13:00:33 +1000

    Cafe Onion image test
    Image test

--- script.js
+++ script.js
@@ -964,7 +964,7 @@ const places = [
         title: "Cafe Onion Anguk",
 
         image:
-            "images/seoul/cafe-onion.jpg",
+            "images/SEOUL/cafe-onion.jpg",
 
         duration:
             "45–60 minutes",
